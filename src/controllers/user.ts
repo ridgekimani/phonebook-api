447c786c13b21183381316bbb3c4e2b4d2c81abd
Ridge Kimani <ridgekimani@gmail.com>
2018-09-22 15:14:13 +0300

    separate schemas

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -2,27 +2,9 @@ import { Router, Request, Response } from 'express';
 import Joi from 'joi';
 import bcyrpt from 'bcrypt';
 import User from '../models/User';
+import userSchema from '../schemas/user.schema';
 
 const router: Router = Router();
-
-const userSchema = Joi.object({
-  firstName: Joi.string()
-    .alphanum()
-    .min(3)
-    .max(30)
-    .required(),
-  lastName: Joi.string()
-    .alphanum()
-    .min(3)
-    .max(30)
-    .required(),
-  email: Joi.string()
-    .email({ minDomainAtoms: 2 })
-    .required(),
-  password: Joi.string()
-    .regex(/^[a-zA-Z0-9]{3,30}$/)
-    .required()
-});
 
 router.route('/').post(async function(req: Request, res: Response) {
   const validator = Joi.validate(req.body, userSchema, {
