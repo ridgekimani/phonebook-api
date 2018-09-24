5da2f837f643e3e438434436b926ffc7529f8218
Ridge Kimani <ridgekimani@gmail.com>
2018-09-25 00:21:27 +0300

    update build process for heroku

--- src/tests/hello.test.ts
+++
@@ -1,8 +0,0 @@
-import request from "supertest"
-import app from "../app"
-
-describe("/ hello url", () => {
-    it("should return 200 OK", () => {
-        return request(app).get('/').expect(200)
-    })
-})
